a2b87979e96544b25259e578f2e2de0302f46a6d
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-01-14 03:42:45 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,3 +3,12 @@ let accountEmail = "rajdeep@google.com"
 var accountPassword = "12345"
 accountCity = "dehradun"
 
+
+console.log("accountId")
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(3 + 3) // we are using nodejs not browser
+
+let name = "Rajdeep"
+let age = 23
+let loggedIn = true
+
+
+// number 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value // like temperature set to null as 0 is valid temperature value
+// undefined => declared but not defined // let age;
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Rajdeep"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof 23); // number
+
+
+
