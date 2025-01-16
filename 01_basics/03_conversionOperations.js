8d6ef54370580585370c49f2ad86ffa61d5e4b86
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-01-16 12:46:06 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -39,10 +39,24 @@ let str1 = "hello"
 let str2 = " rajdeep"
 
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
 
-console.log("1" + 2)
-console.log("1" + 2)
-console.log("1"+ 2 +2);
-console.log("1"+2+"2");
+// console.log("1" + 2) // js will convert int to string
+// console.log("1" + 2)
+// console.log("1"+ 2 +2);   // 122 first data type is string
+// console.log("1"+2+"2");   // 122
+// console.log(1 + 2 + "3"); // 33 string is in last
 
+// console.log(true) // true
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+let x = 3;
+let y = x++;
+console.log(y) // 3
+console.log(x); // 4
+
+let a = 4;
+let b = ++a;
+console.log(b); // 5
+console.log(a); // 5
