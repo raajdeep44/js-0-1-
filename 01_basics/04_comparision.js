16775e9c64e6ce5c6f414bcc03e05930aacf95a7
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-01-16 19:14:51 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log("2" > 1); // comparision convert string to number
+// console.log("02" > 1); 
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+// console.log(null <= 0) // true
+
+// == and comaparisions(<,>,<=,>=) work differently.
+// comparisons convert null to a number, treating it as 0.
+
+// === strict check
+console.log("2" == 2) // true
+console.log("2" === 2) // false --> strictly check the data type
+
