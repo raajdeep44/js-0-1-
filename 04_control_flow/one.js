c4a57d92afe5e386d3455964a9c34e3a62f5ad50
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-02-01 13:11:16 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,9 @@
+ // if 
+// === also checks type // !==
+ const loggedInGoogle = true;
+ const loggedInEmail = false;
+
+ if(loggedInEmail || loggedInGoogle){
+    console.log("user logged in");
+ }
+
