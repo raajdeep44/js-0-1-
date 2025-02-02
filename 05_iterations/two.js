18441bde00e82ec7dbe8ca45bb789110d67e9add
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-02-02 03:20:24 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+
+let index = 0;
+while(index < 10){
+    // console.log(index);
+    index = index + 2;
+}
+
+let myArr = ['js','cpp','python','java'];
+
+let i = 0;
+while(i < myArr.length){
+    // console.log(myArr[i]);
+    i++;
+}
+
+
+let score = 11;
+do{
+    console.log(score);
+    score++;
+}while(score <= 10);
+console.log(score);
