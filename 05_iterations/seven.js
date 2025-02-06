d882f6c78d5f91aa6a16a6bca94ada64941253e1
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-02-06 09:46:09 +0000

    map and chaining

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -6,9 +6,15 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 //     return num + 10
 // })
 
-const newNumbers = []
-myNumbers.forEach( (num)=>{
-    newNumbers.push(num+10)
-})
+// const newNumbers = []
+// myNumbers.forEach( (num)=>{
+//     newNumbers.push(num+10)
+// })
 
-console.log(newNumbers)
+
+// ++++++++++++++++ CHAINING +++++++++++++++++
+const newNumbers = myNumbers
+                    .map((num)=> num * 10)
+                    .map((num)=>num+1)
+                    .filter((num)=> num >=50);
+console.log(newNumbers);
