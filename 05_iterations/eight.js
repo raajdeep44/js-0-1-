6e87c5ac8b549c64c35b989548aaef4d1a37ed35
raajdeep44 <167026606+raajdeep44@users.noreply.github.com>
2025-02-10 05:35:41 +0000

    reduce

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -4,8 +4,33 @@ const myNums = [1,2,3]
 const myTotal = myNums.reduce((acc,currval)=>{
     console.log(`acc :${acc} and current value : ${currval}`)
     return acc + currval
-}, 0)
+},0)
+// const myTotal = myNums.reduce((acc,num)=>{
+//     return acc + num
+// },0)
 // const myTotal = myNums.map((acc,num)=>{
 //     return acc + num;
 // })
-console.log(myTotal);
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemname : "js course",
+        price : 599
+    },
+    {
+        itemname : "data science course",
+        price : 2000
+    },
+    {
+        itemname : "dsa course",
+        price : 1500
+    }
+]
+
+const priceTopay = shoppingCart.reduce((acc,item)=>{
+    return acc + item.price
+},0)
+
+console.log(priceTopay)
